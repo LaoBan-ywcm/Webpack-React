f309ead37765842787981413c33585e40725fef3
LaoBan-ywcm <249007415@qq.com>
2017-07-24 15:56:25 +0800

    搭建本地服务器

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,6 +1,6 @@
 module.exports = function() {
-    var greet = document.createElement('div');
-    greet.textContent = "Hello World!";
+    const greet = document.createElement('div');
+    greet.textContent = "Hello World!!";
     return greet;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-    entry:__dirname + '/app/main.js',
+    entry:__dirname + '/app/main.js',   //入口文件
     output:{
-        path:__dirname + '/public',
-        filename:'bundle.js'
+        path:__dirname + '/public',    //输出路径
+        filename:'bundle.js'            //输出的文件名
     },
-    devtool:"source-map"
+    devtool:"source-map",               //配置source Maps选项
+    devServer:{                         //设置本地服务器
+        contentBase:'./public',         //生成服务器的目录
+        port:"9000",                    //端口号
+        inline:true,                    //当源文件修改时要不要刷新页面
+        historyApiFallback:true,        //HTML5中单页面应用
+    }
 }
